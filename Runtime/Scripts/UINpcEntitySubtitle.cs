263e1890ec38280537ea3c91e7510b01d72d1633
John Money <jemoney@protonmail.com>
2025-12-29 16:48:55 -0500

    revert to partial

--- Runtime/Scripts/UINpcEntitySubtitle.cs
+++ Runtime/Scripts/UINpcEntitySubtitle.cs
@@ -3,60 +3,60 @@
  * Author: Denarii Games
  * Version: 1.0
  *
- * Add component to CanvasNpcUI or use prefab CanvasNpcSubtitleUI.
+ * Replace component on CanvasNpcUI or use prefab CanvasNpcSubtitleUI.
  */
 
 using Cysharp.Text;
 using UnityEngine;
-using NightBlade;
 
-[RequireComponent(typeof(NpcEntity))]
-public class UINpcEntitySubtitle : MonoBehaviour
+namespace NightBlade
 {
-	[Header("NPC Entity - UI Elements")]
-	public TextWrapper uiTextSubtitle;
-
-	[Header("NPC Entity - String Formats")]
-	public UILocaleKeySetting formatKeySubtitle = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_SIMPLE);
-/*
-	protected override void AddEvents(NpcEntity entity)
+	public partial class UINpcEntitySubtitle : UINpcEntity
 	{
-		base.AddEvents(entity);
+		[Header("NPC Entity - UI Elements")]
+		public TextWrapper uiTextSubtitle;
 
-		if (entity == null)
-			return;
-		entity.SyncSubtitle.onChange += OnSyncSubtitleChanged;
+		[Header("NPC Entity - String Formats")]
+		public UILocaleKeySetting formatKeySubtitle = new UILocaleKeySetting(UIFormatKeys.UI_FORMAT_SIMPLE);
+
+		protected override void AddEvents(NpcEntity entity)
+		{
+			base.AddEvents(entity);
+
+			if (entity == null)
+				return;
+			entity.SyncSubtitle.onChange += OnSyncSubtitleChanged;
+		}
+
+		protected override void RemoveEvents(NpcEntity entity)
+		{
+			base.RemoveEvents(entity);
+
+			if (entity == null)
+				return;
+			entity.SyncSubtitle.onChange -= OnSyncSubtitleChanged;
+		}
+
+		protected override void UpdateData()
+		{
+			base.UpdateData();
+			UpdateSubtitle();
+		}
+
+		private void OnSyncSubtitleChanged(bool isInitial, string oldSubTitle, string value)
+		{
+			UpdateSubtitle();
+		}
+
+		protected void UpdateSubtitle()
+		{
+			if (uiTextSubtitle == null)
+				return;
+			string tempTitle = Data == null ? string.Empty : Data.Subtitle;
+			uiTextSubtitle.SetGameObjectActive(!string.IsNullOrEmpty(tempTitle));
+			uiTextSubtitle.text = ZString.Format(
+				LanguageManager.GetText(formatKeySubtitle),
+				tempTitle);
+		}
 	}
-
-	protected override void RemoveEvents(NpcEntity entity)
-	{
-		base.RemoveEvents(entity);
-
-		if (entity == null)
-			return;
-		entity.SyncSubtitle.onChange -= OnSyncSubtitleChanged;
-	}
-
-	protected override void UpdateData()
-	{
-		base.UpdateData();
-		UpdateSubtitle();
-	}
-
-	private void OnSyncSubtitleChanged(bool isInitial, string oldSubTitle, string value)
-	{
-		UpdateSubtitle();
-	}
-
-	protected void UpdateSubtitle()
-	{
-		if (uiTextSubtitle == null)
-			return;
-		string tempTitle = Data == null ? string.Empty : Data.Subtitle;
-		uiTextSubtitle.SetGameObjectActive(!string.IsNullOrEmpty(tempTitle));
-		uiTextSubtitle.text = ZString.Format(
-			LanguageManager.GetText(formatKeySubtitle),
-			tempTitle);
-	}
-*/
 }
